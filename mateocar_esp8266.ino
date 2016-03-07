2d90459a6a122714e0cc8e60d1a3d4da8fb60f07
mtorres <mtorres@iter.es>
2016-03-07 12:21:12 +0000

    wifi AP code and OTA from examples

---
+++ mateocar_esp8266.ino
@@ -0,0 +1,29 @@
+/*
+ * ride-on car remote controller
+ * 
+ */
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+/* Default password, should be changeable through app. */
+const char *ssid = "Mateocar";
+const char *password = "Mateocar";
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+}
+
+void loop() {
+
+}
