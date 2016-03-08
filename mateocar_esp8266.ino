06bed8fab3314ec35ce9365a1873aaa5d4cc966f
migueltorres <miguel.torres@gmail.com>
2016-03-08 08:59:07 +0000

    main loop structure and OTA logic

--- mateocar_esp8266.ino
+++ mateocar_esp8266.ino
@@ -10,6 +10,12 @@
 /* Default password, should be changeable through app. */
 const char *ssid = "Mateocar";
 const char *password = "Mateocar";
+bool boot = true;
+const int OTApin = 1;
+int activeOTApin = 0;
+
+unsigned long previousMillis = 0;
+const long interval = 10;
 
 void setup() {
   delay(1000);
@@ -22,8 +28,58 @@ void setup() {
   Serial.print("AP IP address: ");
   Serial.println(myIP);
 
+  pinMode(OTApin, INPUT);
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
 }
 
 void loop() {
+// check for firmware updates only at boot
+  if (boot) {
+    activeOTApin = digitalRead(OTApin);
+    if (activeOTApin) {  
+      while(1) // wait until update is completed
+        ArduinoOTA.handle();
+    }
+  } else  
+    boot = false;
+  // main loop 100hz
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;   
+  // check for incoming wifi data
 
+    mateocar.parseSerialRC(data);
+    if () // 10Hz telemetry
+      telemetry = mateocar.getTelemetry();
+    // send pending wifi data
+
+  }
 }
